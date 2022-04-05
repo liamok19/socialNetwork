c5c7c4384ec1fdb3c90660a0ea3db305d9e02402
Liam Ok <liamokane19@gmail.com>
2022-04-05 15:28:14 +1000

    updating user model with correct validation and thoughts,reactions reference.

---
+++ models/Thought.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    maxlength: 280,
+    minlength: 1,
+  },
+  reactions: [ReactionSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Thought = model("thought", thoughtSchema);
+
+// module.exports = thoughtSchema;
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,42 +1,60 @@
-const { use } = require('express/lib/application');
-const { Schema, model } = require('mongoose');
+const { use } = require("express/lib/application");
+const { Schema, model } = require("mongoose");
 // import { isEmail } from 'validator';
-const {isEmail} = require('validator');
-
+const { isEmail } = require("validator");
+const Thought = require("./Thought");
 
 const userSchema = new Schema(
-    {
-        userId: {
+  {
+    // userId: {
+    //   type: Schema.Types.ObjectId,
+    //   default: () => new Types.ObjectId(),
+    // },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+
+      maxlength: 50,
+      minlength: 4,
+      default: "Unnamed user",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: {
+        validator: (v) => isEmail(v),
+        message: "Invalid Email",
+      },
+        thoughts: [{
             type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        userName:{
-            item: { type: String, required: true, unique: true, trim: true  },
-            maxlength: 50,
-            minlength: 4,
-            default: 'Unnamed user',
-        },
-        email: {
-            item: { type: String, required: true, unique: true, trim: true },
-            validate: [ isEmail, 'invalid email' ],
-            thoughts: [thoughtSchema],
-            friends: [
-                {
-                    type: Schema.Types.ObjectId, 
-                    ref: 'friend', 
-                },
-            ],
-        }
-        },
+            ref: Thought,
+        }],
+        friends: [
         {
-            toJSON: {
-                virtuals:true
-            },
-            id: false,
-        })
+          type: Schema.Types.ObjectId,
+          ref: "user",
+        },
+      ],
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-userSchema.virtual('friendCount').get(function () {
-    return this.friends.length;
-    });
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
-module.exports = userSchema;
+const User = model("user", userSchema);
+
+module.exports = User;
+
+
