d1d47cdd62b65ae84e49fcf3a1729dc6902c9f1f
Liam Ok <liamokane19@gmail.com>
2022-04-10 11:27:25 +1000

    applied the schema only details required for the reactionSchema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,32 +1,32 @@
 const { Schema, model } = require("mongoose");
 
 
-const reactionSchema = new Schema({
-    reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId(),
-    },
-    reactionBody: {
-        type: String, 
-        required: true, 
-        maxlength: 280,
-    },
-    userName: {
-        type: String,
-        required: true,
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
         },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    toJSON: {
-        virtuals: true,
+        reactionBody: {
+            type: String, 
+            required: true, 
+            maxlength: 280,
         },
-})
+        userName: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
 
-reactionSchema
-.virtual('reactionCount')
-.get(function () {
-    return this.length.reactions;
-
-});
+module.exports = reactionSchema;
