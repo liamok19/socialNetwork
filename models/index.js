f871b87fc5f4c146f55623f51885ae431e422f43
Liam Ok <liamokane19@gmail.com>
2022-04-10 11:31:42 +1000

    required user, thought and reaction into a index.js. then export the 3 into an object

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require('./User');
+const Thought = require('./Thought');
+const Reaction = require('./Reaction');
+
+// console.log(User, 'user obj');
+// console.log(Thought, 'thought obj');
+// console.log(Reaction, 'reaction obj');
+
+module.exports = { User, Thought, Reaction};
