ae9d3f232eba762e9c92cfc1c27bc05c7ded210b
Liam Ok <liamokane19@gmail.com>
2022-04-10 13:21:57 +1000

    calling the api routes for the controller. CRUD details for the thought have been added

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -8,5 +8,15 @@ const {
     deleteUser,
     addReaction,
     removeReaction,
-  } = require('../../controllers/userController');
-  
+} = require('../../controllers/userController');
+
+
+router.route('/').get(getUser).post(createUser);
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+router.route('/:userId/reactions').post(addReaction);
+
+router.route('/:userId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
